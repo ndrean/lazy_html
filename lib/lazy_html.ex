4b2be678f132a46a5408f7ac73623a296ee41f48
Jonatan Kłosko <jonatanklosko@gmail.com>
2025-03-24 13:25:19 +0900

    Initial commit

---
+++ lib/lazy_html.ex
@@ -0,0 +1,18 @@
+defmodule LazyHtml do
+  @moduledoc """
+  Documentation for `LazyHtml`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> LazyHtml.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/lazy_html_test.exs
@@ -0,0 +1,8 @@
+defmodule LazyHtmlTest do
+  use ExUnit.Case
+  doctest LazyHtml
+
+  test "greets the world" do
+    assert LazyHtml.hello() == :world
+  end
+end
