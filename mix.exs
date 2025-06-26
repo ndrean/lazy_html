ceca1f9767ce50d20aef22464d889bd1dc5cbd49
Jonatan Kłosko <jonatanklosko@gmail.com>
2025-06-26 12:50:41 +0200

    Update version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule LazyHTML.MixProject do
 
   @lexbor_version "2.4.0"
 
-  @version "0.1.3"
+  @version "0.1.4-dev"
   @description "Efficient parsing and querying of HTML documents"
   @github_url "https://github.com/dashbitco/lazy_html"
 
